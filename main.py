bd1680ab2a308d8ec8bd2fac5076678f46bcb3fe
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-07-28 14:46:26 -0300

    refactor interface so it is easier to copy commands

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from colorama import Fore, Back, Style
 def formatToSingleLine():
     with open('command.txt') as f:
         lines = f.readlines()
+        f.close()
 
     command = ''
     for line in lines:
         command += line.strip() + ' '
+
+    f = open("formattedToSingleLine.txt", "a")
+    f.write(command)
+    f.close()
 
     return command
 
@@ -17,7 +22,8 @@ while True:
         "Hello, what would you like to do?\n" + "1. Format a curl command to a single line\n" + Fore.RED + "2. Exit\n" + Fore.RESET))
 
     if wish == 1:
-        print(Fore.YELLOW + formatToSingleLine() + Fore.RESET)
+        formatToSingleLine()
+        print(Fore.GREEN + "Done!\n" + Fore.RESET)
 
     if wish == 2:
         break
