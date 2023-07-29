cfabe15a4f8e61ec7f73e8ce02f051b76ddea044
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-07-29 14:29:48 -0300

    add encoding system because i dont trust websites to do it for me

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from colorama import Fore, Back, Style
+import base64
 
 
 def formatToSingleLine():
@@ -17,13 +18,32 @@ def formatToSingleLine():
     return command
 
 
+def convertToBase64():
+    with open('token.txt') as f:
+        token = f.readlines()
+        f.close()
+
+    token = token[0].strip()
+    token_bytes = token.encode('ascii')
+
+    base64_token = base64.b64encode(token_bytes).decode('utf-8')
+
+    f = open("encodeTokenToBase64.txt", "a")
+    f.write(base64_token)
+    f.close()
+
+    return base64_token
+
+
 while True:
     wish = int(input(
-        "Hello, what would you like to do?\n" + "1. Format a curl command to a single line\n" + Fore.RED + "2. Exit\n" + Fore.RESET))
+        "Hello, what would you like to do?\n" + "1. Format a curl command to a single line\n" + "2. Encode API Token to Base64\n" + Fore.RED + "3. Exit\n" + Fore.RESET))
 
     if wish == 1:
         formatToSingleLine()
         print(Fore.GREEN + "Done!\n" + Fore.RESET)
-
-    if wish == 2:
+    elif wish == 2:
+        convertToBase64()
+        print(Fore.GREEN + "Encoded!\n" + Fore.RESET)
+    elif wish == 3:
         break
